3e92f92a358803a17d6709af25f93ccd8b38eb00
aashi21996 <aashi.kankane21996@gmail.com>
2026-09-11 10:12:30 +0530

    try catch

--- scopeClosures/closureConcept.js
+++ scopeClosures/closureConcept.js
@@ -14,34 +14,7 @@ myCounter(); // Output: 1
 myCounter(); // Output: 2
 myCounter(); // Output: 3
 
-//
-function createBankAccount() {
-    let balance = 1000; // Yeh private variable hai, ise bahar se koi direct access nahi kar sakta
+const service = myCounter();
+console.log (myCounter);
 
-    return {
-        deposit: function(amount) {
-            balance += amount;
-            console.log(`Deposited: ${amount}. Total Balance: ${balance}`);
-        },
-        withdraw: function(amount) {
-            if (amount <= balance) {
-                balance -= amount;
-                console.log(`Withdrawn: ${amount}. Remaining: ${balance}`);
-            } else {
-                console.log("Balance kam hai!");
-            }
-        },
-        getBalance: function() {
-            return balance;
-        }
-    };
-}
 
-const myAccount = createBankAccount();
-
-myAccount.deposit(500);  // Output: Deposited: 500. Total Balance: 1500
-myAccount.withdraw(200); // Output: Withdrawn: 200. Remaining: 1300
-
-//  Tum balance ko direct badal nahi sakte:
-// console.log(balance); // Error! `balance` is not defined outside.
-console.log(myAccount.getBalance()); // Output: 1300 (Sirf function ke zariye dekh sakte hain)
